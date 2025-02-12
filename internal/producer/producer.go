dcc2e3aa77724b08b3d48119f75ab727b237551b
Александр Закиров <zakirov_ai@ekb.sima-land.ru>
2025-02-12 15:15:58 +0500

    чето происходит

--- internal/producer/producer.go
+++ internal/producer/producer.go
@@ -2,10 +2,8 @@ package producer
 
 import (
 	"context"
-	"log"
-	"time"
-
 	"github.com/segmentio/kafka-go"
+	"log"
 )
 
 func Producer(topic string, brocker string) {
@@ -24,8 +22,8 @@ func Producer(topic string, brocker string) {
 	log.Println("Loop started")
 	for i := 0; i < 10; i++ {
 		msg := kafka.Message{
-			Key:   []byte("Key"),
-			Value: []byte("Привет, Kafka! " + time.Now().Format(time.RFC3339)),
+			Key:   []byte("ZXC"),
+			Value: []byte("Привет, KaKafka!" + string(rune(i))),
 		}
 		err := writer.WriteMessages(context.Background(), msg)
 		log.Printf("Producer produced message to topic %s\n", msg.Topic)
@@ -33,6 +31,5 @@ func Producer(topic string, brocker string) {
 			log.Fatalf("Ошибка при отправке сообщения: %v", err)
 		}
 		log.Printf("Отправлено сообщение: %s", msg.Value)
-		time.Sleep(2 * time.Second)
 	}
 }
